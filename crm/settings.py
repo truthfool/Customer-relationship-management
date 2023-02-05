b74a778e5aafddfa1c2e7d9b2ca0534474c6f546
truthfool <ishanranasingh@gmail.com>
2023-02-05 17:37:53 +0530

    env file

--- crm/settings.py
+++ crm/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*omi8o=kvy%6j7d--co7it!^^cuvv29ywi^)myk#)b(@yq_wap"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
-Google and Facebook Sign in and register
-CI/CD pipeline setup
-Deploy
-Unit tests
-APIs using DRF
+# Customer Relationship Management
+
+A CRM(Customer Relationship Management) built using django.
+A customer can create its account and track its orders on the users page. The admin can view all the customers details and orders placed and status of the order.
+
+Functionalities :
+
+1. Authentication using username email and password.
+2. Authorization levels of admin and customer.
+3. Dashboard page to track all customers and their orders.
+4. Products placed for each individual customer.
+5. Password reset using email.
